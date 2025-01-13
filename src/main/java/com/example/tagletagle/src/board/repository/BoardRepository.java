e7ed35c603af6b27db66f476fed0721a1838a47c
eclipse021 <yahang2008@naver.com>
2025-01-13 16:44:13 +0900

    Feat : 닉네임 중복 검사, 태그 전체 목록 조회 API 추가

--- src/main/java/com/example/tagletagle/src/board/repository/BoardRepository.java
+++ src/main/java/com/example/tagletagle/src/board/repository/BoardRepository.java
@@ -2,10 +2,16 @@ package com.example.tagletagle.src.board.repository;
 
 import javax.sql.DataSource;
 
+import com.example.tagletagle.src.board.entity.PostEntity;
+import com.example.tagletagle.src.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
+public interface BoardRepository extends JpaRepository<PostEntity, Long> {
 
 
 }
--- src/main/java/com/example/tagletagle/src/board/service/BoardService.java
+++ src/main/java/com/example/tagletagle/src/board/service/BoardService.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Service;
 
 import com.example.tagletagle.src.board.repository.BoardRepository;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class BoardService {
 
 	private final BoardRepository boardRepository;
